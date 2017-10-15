1ba83fdc1bfdc9edbcb9571cac8a3d6aae6bda75
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2017-10-15 21:16:56 +0200

    Error handling.

--- api/server.go
+++ api/server.go
@@ -4,6 +4,8 @@ import (
 	fmt "fmt"
 
 	context "golang.org/x/net/context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type server struct{}
@@ -13,6 +15,10 @@ func New() GoGraphQLServer {
 }
 
 func (svc *server) SayHello(ctx context.Context, req *SayHelloReq) (*SayHelloRes, error) {
+	if req.Name == "" {
+		return nil, status.Error(codes.InvalidArgument, "name cannot be blank")
+	}
+
 	msg := fmt.Sprintf("Hello %s", req.Name)
 
 	return &SayHelloRes{
